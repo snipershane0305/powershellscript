21f1bd20ff400f0d7313636d56c7c13462149cab
snipershane0305 <snipershane0305@gmail.com>
2025-03-06 19:06:18 -0500

    Create cleanmem.ps1

---
+++ programs/cleanmem.ps1
@@ -0,0 +1,3 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process pwsh.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+Set-Location $env:SystemDrive\
+.\memreduct.exe -clean:full

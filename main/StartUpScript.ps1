c1eaca7c948e9511f9492d13d6f717bd09427453
snipershane0305 <snipershane0305@gmail.com>
2025-11-09 11:48:56 +0000

    change to $env:SystemDrive

--- main/StartUpScript.ps1
+++ main/StartUpScript.ps1
@@ -1,4 +1,3 @@
 #put this in your startup folder
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process pwsh.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-$systemDrive = (Get-WmiObject -Class Win32_OperatingSystem).SystemDrive
-start-process pwsh.exe -WindowStyle Minimized "$systemDrive\PowershellScript.ps1"
+start-process pwsh.exe -WindowStyle Minimized "$env:SystemDrive\PowershellScript.ps1"

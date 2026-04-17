7ada69294f55e57df30e992b91a7b325a6a84439
snipershane0305 <snipershane0305@gmail.com>
2026-04-17 03:47:50 -0400

    Delete programs/cleanmem.ps1

--- programs/cleanmem.ps1
+++
@@ -1,3 +0,0 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process pwsh.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-Set-Location $env:SystemDrive\
-.\memreduct.exe -clean:full
